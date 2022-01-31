5a5d127570c46fb725026590810740cd8d02d332
Sergey <sergey.hudzenko@orderin.co.za>
2022-01-31 03:23:08 +0200

    test

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -98,7 +98,7 @@ class MainPageController extends Controller
      */
     public function show($login)
     {
-        dd($this->redis->command());
+        dd($this->redis->command('HEXISTS'));
         // If Reddis keys exist hash of serched users -> return  users and repos from cache
         if ($this->redis->hExists('user', $login) && $this->redis->hExists('repos', $login)) {
             $rUser = json_decode($this->redis->hGet('user', $login));
